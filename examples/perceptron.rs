5ac2f0a146b604c8542eb11f40ab60841de99f0d
Victor Rodriguez <awtgerry@gmail.com>
2023-10-29 20:54:54 -0600

    perceptron

--- examples/perceptron.rs
+++ examples/perceptron.rs
@@ -1,142 +1,70 @@
 use rand::Rng;
 
-struct NeuralNetwork {
-    input_nodes: usize,
-    hidden_nodes: usize,
-    output_nodes: usize,
+struct Perceptron {
+    weights: [f64; 2],
+    bias: f64,
     learning_rate: f64,
-    weights_ih: Vec<Vec<f64>>,
-    weights_ho: Vec<f64>,
-    bias_h: Vec<f64>,
-    bias_o: f64,
 }
 
-impl NeuralNetwork {
-    fn new(input_nodes: usize, hidden_nodes: usize, output_nodes: usize, learning_rate: f64) -> Self {
+impl Perceptron {
+    fn new() -> Self {
         let mut rng = rand::thread_rng();
+        let weights = [rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0)];
+        let bias = rng.gen_range(-1.0..1.0);
+        let learning_rate = 0.01;
 
-        let weights_ih: Vec<Vec<f64>> = (0..hidden_nodes)
-            .map(|_| (0..input_nodes)
-                .map(|_| rng.gen_range(-1.0..1.0))
-                .collect())
-            .collect();
-
-        let weights_ho: Vec<f64> = (0..output_nodes)
-            .map(|_| rng.gen_range(-1.0..1.0))
-            .collect();
-
-        let bias_h: Vec<f64> = (0..hidden_nodes)
-            .map(|_| rng.gen_range(-1.0..1.0))
-            .collect();
-
-        let bias_o = rng.gen_range(-1.0..1.0);
-
-        NeuralNetwork {
-            input_nodes,
-            hidden_nodes,
-            output_nodes,
+        Perceptron {
+            weights,
+            bias,
             learning_rate,
-            weights_ih,
-            weights_ho,
-            bias_h,
-            bias_o,
         }
     }
 
-    fn sigmoid(x: f64) -> f64 {
-        1.0 / (1.0 + (-x).exp())
+    fn activate(&self, sum: f64) -> i32 {
+        if sum > 0.0 {
+            1
+        } else {
+            0
+        }
     }
 
-    fn sigmoid_derivative(x: f64) -> f64 {
-        x * (1.0 - x)
+    fn feed_forward(&self, inputs: &[i32; 2]) -> i32 {
+        let weighted_sum = (inputs[0] as f64 * self.weights[0])
+            + (inputs[1] as f64 * self.weights[1])
+            + self.bias;
+        self.activate(weighted_sum)
     }
 
-    fn feed_forward(&self, input: &[f64; 2]) -> f64 {
-        // Capa de entrada a capa oculta
-        let mut hidden = vec![0.0; self.hidden_nodes];
-        for i in 0..self.hidden_nodes {
-            hidden[i] = self.bias_h[i];
-            for j in 0..self.input_nodes {
-                hidden[i] += input[j] * self.weights_ih[i][j];
-            }
-            hidden[i] = NeuralNetwork::sigmoid(hidden[i]);
+    fn train(&mut self, inputs: &[i32; 2], target: i32) {
+        let guess = self.feed_forward(inputs);
+        let error = target - guess;
+        for i in 0..2 {
+            self.weights[i] += self.learning_rate * (error as f64) * (inputs[i] as f64);
         }
-
-        // Capa oculta a capa de salida
-        let mut output = 0.0;
-        for i in 0..self.output_nodes {
-            output += hidden[i] * self.weights_ho[i];
-        }
-
-        NeuralNetwork::sigmoid(output)
-    }
-
-    fn train(&mut self, input: &[f64; 2], target: f64) {
-        // Paso hacia adelante
-        // Capa de entrada a capa oculta
-        let mut hidden = vec![0.0; self.hidden_nodes];
-        for i in 0..self.hidden_nodes {
-            hidden[i] = self.bias_h[i];
-            for j in 0..self.input_nodes {
-                hidden[i] += input[j] * self.weights_ih[i][j];
-            }
-            hidden[i] = NeuralNetwork::sigmoid(hidden[i]);
-        }
-
-        // Capa oculta a capa de salida
-        let mut output = 0.0;
-        for i in 0..self.output_nodes {
-            output += hidden[i] * self.weights_ho[i];
-        }
-
-        let predicted = NeuralNetwork::sigmoid(output);
-
-        // Calcular el error
-        let error = target - predicted;
-
-        // Paso hacia atrás (retropropagación)
-        // Capa de salida a capa oculta
-        let output_gradient = error * NeuralNetwork::sigmoid_derivative(predicted);
-        for i in 0..self.output_nodes {
-            self.bias_o += self.learning_rate * output_gradient;
-            for j in 0..self.hidden_nodes {
-                self.weights_ho[i] += self.learning_rate * output_gradient * hidden[j];
-            }
-        }
-
-        // Capa oculta a capa de entrada
-        let mut hidden_gradient = vec![0.0; self.hidden_nodes];
-        for i in 0..self.hidden_nodes {
-            hidden_gradient[i] = 0.0;
-            for j in 0..self.output_nodes {
-                hidden_gradient[i] += output_gradient * self.weights_ho[j];
-            }
-            hidden_gradient[i] *= NeuralNetwork::sigmoid_derivative(hidden[i]);
-            for j in 0..self.input_nodes {
-                self.weights_ih[i][j] += self.learning_rate * hidden_gradient[i] * input[j];
-            }
-            self.bias_h[i] += self.learning_rate * hidden_gradient[i];
-        }
+        self.bias += self.learning_rate * (error as f64);
     }
 }
 
 fn main() {
-    let mut nn = NeuralNetwork::new(2, 2, 1, 0.1);
-    let training_data = [
-        ([0.0, 0.0], 0.0),
-        ([0.0, 1.0], 1.0),
-        ([1.0, 0.0], 1.0),
-        ([1.0, 1.0], 0.0),
+    // Perceptrón para la compuerta lógica AND
+    let mut and_perceptron = Perceptron::new();
+    let and_training_data = [
+        ([0, 0], 0),
+        ([0, 1], 0),
+        ([1, 0], 0),
+        ([1, 1], 1),
     ];
 
+    // Entrenamiento para la compuerta lógica AND
     for _ in 0..10000 {
-        for &(inputs, target) in &training_data {
-            nn.train(&inputs, target);
+        for &(inputs, target) in &and_training_data {
+            and_perceptron.train(&inputs, target);
         }
     }
 
-    for &(inputs, _) in &training_data {
-        let result = nn.feed_forward(&inputs);
-        println!("{:?} -> {:.4}", inputs, result);
+    // Pruebas para AND
+    for &(inputs, _) in &and_training_data {
+        let result = and_perceptron.feed_forward(&inputs);
+        println!("AND {:?} -> {}", inputs, result);
     }
 }
